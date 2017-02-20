f39a8e675906bd64f9b3e0d7fd98cc388ec45c73
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-02-20 22:54:40 +0000

    basic operation

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 'use strict'
 const Lws = require('../')
-const lws = new Lws()
-lws.start()
+const Spa = require('local-web-server-spa')
+const Static = require('local-web-server-static')
+const lws = new Lws({
+  stack: [ Spa, Static, 'server/websocket-server.js' ],
+  port: 8010,
+  spa: 'index.html'
+})
+lws.listen()
