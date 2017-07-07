b8855a41239e5d3df700d642ca9142ebfd2f9aee
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-07-07 23:41:52 +0100

    tests

--- test/lws.js
+++ test/lws.js
@@ -1,5 +1,6 @@
 'use strict'
 const TestRunner = require('test-runner')
+const Counter = require('test-runner-counter')
 const Lws = require('../')
 const a = require('assert')
 const request = require('req-then')
@@ -23,9 +24,70 @@ runner.test('lws: simple http', async function () {
     stack: [ One ],
     port: port
   })
-  const url = require('url')
   const response = await request(`http://127.0.0.1:${port}`)
   server.close()
   a.strictEqual(response.res.statusCode, 200)
   a.strictEqual(response.data.toString(), 'one')
+})
+
+runner.test('lws: hostname', async function () {
+  const port = 9100 + this.index
+  const One = Base => class extends Base {
+    middleware (options) {
+      return (ctx, next) => {
+        const fs = require('fs')
+        ctx.body = fs.createReadStream('package.json')
+        next()
+      }
+    }
+  }
+  const lws = new Lws()
+  const server = lws.create({
+    stack: [ One ],
+    port: port,
+    hostname: 'localhost'
+  })
+
+  try {
+    await request(`http://127.0.0.1:${port}`)
+    a.fail("shouldn't reach here")
+  } catch (err) {
+  }
+
+  try {
+    const response = await request(`http://localhost:${port}`)
+    a.strictEqual(response.res.statusCode, 200)
+    server.close()
+  } catch (err) {
+    a.fail("shouldn't reach here")
+  }
+
+})
+
+runner.test('lws: --websocket', async function () {
+  const counter = Counter.create(1)
+  const port = 9100 + this.index
+  const One = Base => class extends Base {
+    websocket (wss) {
+      wss.on('connection', ws => {
+        ws.on('message', data => {
+          counter.pass()
+        })
+      })
+    }
+  }
+  const lws = new Lws()
+  const server = lws.create({
+    websocket: One,
+    port: port
+  })
+  const WebSocket = require('ws')
+  const ws = new WebSocket(`ws://127.0.0.1:${port}`)
+
+  ws.on('open', function open() {
+    ws.send('something')
+    server.close()
+    ws.close()
+  })
+  return counter.promise
 })
