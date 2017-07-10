6951694c44861e026bb624a10fdf7d1b811e7562
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-07-10 21:10:28 +0100

    default port.. exception handling

--- test/cli.js
+++ test/cli.js
@@ -1,4 +1,5 @@
 const TestRunner = require('test-runner')
+const Counter = require('test-runner-counter')
 const a = require('assert')
 const CliApp = require('../lib/cli-app')
 const request = require('req-then')
@@ -18,13 +19,32 @@ runner.test('cli.run', async function () {
   a.strictEqual(response.res.statusCode, 404)
 })
 
-runner.test('cli.run: bad option', async function () {
+runner.test('cli.run: bad option, should fail and printError', async function () {
   const port = 7500 + this.index
   const origArgv = process.argv.slice()
   process.argv = [ 'node', 'something', '--should-fail' ]
   const server = CliApp.run()
   process.argv = origArgv
   a.strictEqual(server, undefined)
+})
+
+runner.test('cli.run: port not available', async function () {
+  const counter = Counter.create(1)
+  const port = 7500 + this.index
+  const origArgv = process.argv.slice()
+  process.argv = [ 'node', 'something', '--port', `${port}` ]
+  const server = CliApp.run()
+  server.on('error', err => {
+    counter.fail('should not reach here')
+  })
+  const server2 = CliApp.run()
+  server2.on('error', err => {
+    counter.pass('should fail')
+    server.close()
+    server2.close()
+  })
+  process.argv = origArgv
+  return counter.promise
 })
 
 runner.test('cli.run: --help', async function () {
