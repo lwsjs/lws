e43cd23534a175abb1f24df25dffe8001524ad0e
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-07-19 12:10:35 +0100

    added --pfc, --key and --cert tests

--- test/https.js
+++ test/https.js
@@ -8,7 +8,7 @@ usage.disable()
 
 const runner = new TestRunner()
 
-runner.test('https', async function () {
+runner.test('https: --https', async function () {
   const port = 9200 + this.index
   const One = Base => class extends Base {
     middleware (options) {
@@ -22,6 +22,57 @@ runner.test('https', async function () {
   const server = lws.listen({
     stack: [ One ],
     https: true,
+    port: port
+  })
+  const url = require('url')
+  const reqOptions = url.parse(`https://127.0.0.1:${port}`)
+  reqOptions.rejectUnauthorized = false
+  const response = await request(reqOptions)
+  server.close()
+  a.strictEqual(response.res.statusCode, 200)
+  a.strictEqual(response.data.toString(), 'one')
+})
+
+runner.test('https: --key and --cert', async function () {
+  const port = 9200 + this.index
+  const One = Base => class extends Base {
+    middleware (options) {
+      return (ctx, next) => {
+        ctx.body = 'one'
+        next()
+      }
+    }
+  }
+  const lws = new Lws()
+  const server = lws.listen({
+    stack: [ One ],
+    key: 'ssl/private-key.pem',
+    cert: 'ssl/lws-cert.pem',
+    port: port
+  })
+  const url = require('url')
+  const reqOptions = url.parse(`https://127.0.0.1:${port}`)
+  reqOptions.rejectUnauthorized = false
+  const response = await request(reqOptions)
+  server.close()
+  a.strictEqual(response.res.statusCode, 200)
+  a.strictEqual(response.data.toString(), 'one')
+})
+
+runner.test('https: --pfx', async function () {
+  const port = 9200 + this.index
+  const One = Base => class extends Base {
+    middleware (options) {
+      return (ctx, next) => {
+        ctx.body = 'one'
+        next()
+      }
+    }
+  }
+  const lws = new Lws()
+  const server = lws.listen({
+    stack: [ One ],
+    pfx: 'ssl/lws.pfx',
     port: port
   })
   const url = require('url')
