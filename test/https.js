88c24f1b745d5e523e8378c2476da5cd3d219bf7
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-07-19 12:17:41 +0100

    --max-connections and --keep-alive-timeout tests

--- test/https.js
+++ test/https.js
@@ -83,3 +83,30 @@ runner.test('https: --pfx', async function () {
   a.strictEqual(response.res.statusCode, 200)
   a.strictEqual(response.data.toString(), 'one')
 })
+
+runner.test('https: --pfx, --max-connections, --keep-alive-timeout', async function () {
+  const port = 9200 + this.index
+  const One = Base => class extends Base {
+    middleware (options) {
+      return (ctx, next) => {
+        ctx.body = 'one'
+        next()
+      }
+    }
+  }
+  const lws = new Lws()
+  const server = lws.listen({
+    stack: [ One ],
+    pfx: 'ssl/lws.pfx',
+    port: port,
+    maxConnections: 10,
+    keepAliveTimeout: 10000
+  })
+  const url = require('url')
+  const reqOptions = url.parse(`https://127.0.0.1:${port}`)
+  reqOptions.rejectUnauthorized = false
+  const response = await request(reqOptions)
+  server.close()
+  a.strictEqual(response.res.statusCode, 200)
+  a.strictEqual(response.data.toString(), 'one')
+})
