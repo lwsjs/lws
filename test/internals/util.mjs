08eeb1564db9aab713577b4a5ca5e1417f96cefc
Lloyd Brookes <lloyd.brookes@gmail.com>
2021-06-02 11:00:27 +0100

    fixed internal tests

--- test/internals/util.mjs
+++ test/internals/util.mjs
@@ -1,18 +1,21 @@
-const Tom = require('test-runner').Tom
-const a = require('assert').strict
-const util = require('../../lib/util')
+import TestRunner from 'test-runner'
+import assert from 'assert'
+import { getIPList } from '../../lib/util.mjs'
 
-const tom = module.exports = new Tom()
+const a = assert.strict
+const tom = new TestRunner.Tom()
 
 tom.test('getIPList', async function () {
-  const result = util.getIPList()
+  const result = getIPList()
   a.ok(result.length > 1)
   a.ok(result.every(i => i.name && i.address))
 })
 
 tom.test('getIPList(boundHostname)', async function () {
-  const result = util.getIPList('test')
+  const result = getIPList('test')
   a.equal(result.length, 1)
   a.equal(result[0].name, 'hostname')
   a.ok(result.every(i => i.name && i.address))
 })
+
+export default tom
